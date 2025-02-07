e979df371ee2fd22fbcdd4c5119422bdbb2d4f59
hugofsl <hugofsl@gmail.com>
2025-02-07 16:54:07 -0300

    Update script.js

--- javascript/ex001/script.js
+++ javascript/ex001/script.js
@@ -1,19 +1,18 @@
 
 function pageLoad(){
-const data = new Date(); // Fix the method to get the current date
-}
+const data = new Date(); 
 let hora = data.getHours();
 if (hora < 12) {
     document.querySelector("body").style.backgroundColor = "rgb(238, 230, 182)";
-    document.querySelector("h1").style.color = "var(--cor_titulo_p)"; // Correct the CSS variable usage
+    document.querySelector("h1").style.color = "var(--cor_titulo_p)"; 
     document.getElementById("texto").innerHTML = `<p>Bom dia! Agora são ${hora} horas.</p>`;
     document.getElementsByClassName("imagem")[0].src = "imgs/manha.jpg";
 } else if (hora < 18) {
     document.querySelector("body").style.backgroundColor = "rgb(214, 173, 119)";
     document.getElementById("texto").innerHTML = `<p>Boa Tarde! Agora são ${hora} horas.</p>`;
-    document.getElementsByClassName("imagem")[0].src = "imgs/tarde.jpg"; // Correct the image source
+    document.getElementsByClassName("imagem")[0].src = "imgs/tarde.jpg"; 
 } else if (hora < 24) {
-    document.querySelector("body").style.backgroundColor = "var(--cor_fundo_01)"; // Correct the CSS variable usage
+    document.querySelector("body").style.backgroundColor = "var(--cor_fundo_01)"; 
     document.getElementById("texto").innerHTML = `<p>Boa Noite! Agora são ${hora} horas.</p>`;
     document.getElementsByClassName("imagem")[0].src = "imgs/noite.png";
 } else {
@@ -21,3 +20,5 @@ if (hora < 12) {
     document.getElementById("texto").innerHTML = `<p>Boa Madrugada! Agora são ${hora} horas.</p>`;
     document.getElementsByClassName("imagem")[0].src = "imgs/madrugada.PNG";
 }
+}
+
